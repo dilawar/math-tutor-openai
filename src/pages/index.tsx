42a8f6ca6810b16d4cd29907a3bc545ae6e12fa9
Dilawar Singh <dilawar@subcom.tech>
2024-11-18 09:45:50 +0530

    Got image uploader working
    Now upload the image to AI and get the OCR done.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 // This is what I found on npmjs.com. Not sure if it is great or not!
-import { UploadButton } from '@bytescale/upload-widget-react';
+import { UploadDropzone } from '@bytescale/upload-widget-react';
 
 import { api } from '~/utils/api';
 
-const options = { apiKey: 'free', maxFileCount: 1 };
+const options = { apiKey: 'free', maxFileCount: 1, width: "600px", height: "375px"};
 
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: 'from tRPC' });
@@ -21,22 +21,24 @@ export default function Home() {
       </Head>
 
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <div className="container flex flex-col items-center justify-center gap-5 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Math Made <span className="text-[hsl(280,100%,70%)]">Easy</span>
           </h1>
+          <div className="text text-white">
+              Take a photo of your math problem and get step-by-step solutions instantly
+          </div>
         </div>
 
         <div className="flex justify-center">
-          <UploadButton
+          <UploadDropzone
             options={options}
-            onComplete={(files) => alert(files.map((x) => x.fileUrl).join('\n'))}>
-            {({ onClick }) => (
-              <button onClick={onClick} className="btn btn-primary text-2xl text-white">
-                Upload a file...
-              </button>
-            )}
-          </UploadButton>
+            onUpdate={({ uploadedFiles }) => {
+              console.log('Uploading ', uploadedFiles.map((x) => x.fileUrl).join('\n'));
+            }}
+            width="600px"
+            height="375px"
+          />
         </div>
       </main>
     </>
