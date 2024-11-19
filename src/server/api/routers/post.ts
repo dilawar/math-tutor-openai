53f904f1eb9fa0019dd6e10d426b26d6ace2bf12
Dilawar Singh <dilawar@subcom.tech>
2024-11-19 08:46:10 +0530

    FormData is experimental
    I am going to use upload to R2 bucket and use the URLs.

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,39 +1,48 @@
-import { z } from "zod";
-
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from 'zod';
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+  hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
+    return {
+      greeting: `Hello ${input.text}`
+    };
+  }),
 
- upload: publicProcedure
-    .mutation(async (image) => {
-        console.log("Got image", image);
-        return {
-            msg: "Needs to handle image",
-        };
-    }),
+  upload: publicProcedure.mutation(async (opts) => {
+    // entries in the FormData.
+    console.log("path", opts.path);
+    const formData = await opts.getRawInput();
+    console.log("form data", formData);
+
+    let reader = new FileReader();
+    for(var image of formData.values()) {
+        console.log("form data image: ", image.data);
+        const b64 = reader.readAsDataURL(image);
+        console.log(" image b64: ", b64);
+    }
+
+
+
+    return {
+      msg: 'Got file'
+    };
+  }),
 
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
       return ctx.db.post.create({
         data: {
-          name: input.name,
-        },
+          name: input.name
+        }
       });
     }),
 
   getLatest: publicProcedure.query(async ({ ctx }) => {
     const post = await ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
+      orderBy: { createdAt: 'desc' }
     });
 
     return post ?? null;
-  }),
+  })
 });
---
+++ src/utils/schemas.ts
@@ -0,0 +1,8 @@
+// Thanks https://github.com/trpc/examples-next-formdata/blob/main/src/utils/schemas.ts
+
+import { zfd } from 'zod-form-data';
+
+export const uploadFileSchema = zfd.formData({
+  name: zfd.text(),
+  image: zfd.file(),
+});
