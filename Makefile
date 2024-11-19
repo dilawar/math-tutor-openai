ebd82ef75cb62f68a4317e3373a6b8b7ef04cdc8
Dilawar Singh <dilawar@subcom.tech>
2024-11-20 02:48:49 +0530

    A few lint fixes

--- Makefile
+++ Makefile
@@ -6,6 +6,9 @@ bootstrap:
 	npm run db:generate
 	npm run db:migrate
 
+lint check:
+	npm run lint
+
 .env: .env.gpg
 	@echo "Passphrase is (ask the developer)"
 	/usr/bin/gpg -d $< -o $@
--- src/app/_components/file_upload.tsx
+++ src/app/_components/file_upload.tsx
@@ -5,8 +5,6 @@ import Script from 'next/script';
 // This is what I found on npmjs.com. Not sure if it is great or not!
 import { FileUpload } from 'primereact/fileupload';
 import { Toast } from 'primereact/toast';
-import { Tooltip } from 'primereact/tooltip';
-import { Tag } from 'primereact/tag';
 
 import { useState } from 'react';
 import { api } from '~/trpc/react';
